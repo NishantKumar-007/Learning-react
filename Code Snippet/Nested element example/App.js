3cb998456294b2b38372e2492fb07215efa00f16
NishantKumar-007 <ursnishant4u@gmail.com>
2023-09-26 00:13:15 +0530

    added react through node modules

--- Code Snippet/Nested element example/App.js
+++ Code Snippet/Nested element example/App.js
@@ -1,10 +1,14 @@
-        //nested html structure
+        import React from "react";
+        import ReactDOM from "react-dom";
+       
+       
+       //nested html structure
 
 
         const parent = React.createElement("div",{id: "parent"},
         React.createElement("div",{id: "child1"},
         React.createElement("div",{id: "child2"},
-        [React.createElement("h1",{id: "sibling1"},"Sibling 1 says Hi"), React.createElement("h1",{id: "Sibling2"},"Sibling 2 says Hi")])));
+        [React.createElement("h1",{id: "sibling1"},"Sibling 2 is cool"), React.createElement("h1",{id: "Sibling2"},"Sibling 2 says Hi")])));
         // to create siblings we use [] array 
         // creating stuctures like this is tiresome and ugly that's why we use JSX 
 
