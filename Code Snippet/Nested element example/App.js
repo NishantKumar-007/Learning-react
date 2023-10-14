666ffed31c6dd63950d38f349b133aa33a05e6ea
NishantKumar-007 <ursnishant4u@gmail.com>
2023-10-14 19:49:08 +0530

    lectures 5,6,7 added

--- Code Snippet/Nested element example/App.js
+++ Code Snippet/Nested element example/App.js
@@ -1,18 +1,23 @@
-        import React from "react";
-        import ReactDOM from "react-dom";
-       
-       
-       //nested html structure
+import React from "react";
+import ReactDOM from "react-dom";
 
+//nested html structure
 
-        const parent = React.createElement("div",{id: "parent"},
-        React.createElement("div",{id: "child1"},
-        React.createElement("div",{id: "child2"},
-        [React.createElement("h1",{id: "sibling1"},"Sibling 2 is cool"), React.createElement("h1",{id: "Sibling2"},"Sibling 2 says Hi")])));
-        // to create siblings we use [] array 
-        // creating stuctures like this is tiresome and ugly that's why we use JSX 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child1" },
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", { id: "sibling1" }, "Sibling 2 is cool"),
+      React.createElement("h1", { id: "Sibling2" }, "Sibling 2 says Hi"),
+    ])
+  )
+);
+// to create siblings we use [] array
+// creating stuctures like this is tiresome and ugly that's why we use JSX
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-
-        root.render(parent); 
+root.render(parent);
